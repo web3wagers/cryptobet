dc6c821f6b78995c75903798df3a5f1cea56d18b
adrianvrj <adrivargas48@gmail.com>
2024-07-30 13:12:13 -0600

    [fix] fetching images from contrac

--- src/contracts/BetsContract.ts
+++ src/contracts/BetsContract.ts
@@ -8,7 +8,7 @@ var web3 = new Web3("https://sepolia.optimism.io");
 const defaultAccount = '0xc3798d303a3d0Cbff6f78015c20cf06221c6FF36';
 
 // Create a new contract object using the ABI and address
-const deployedAddress = '0x0c0551e5a1805d93361587cd8C44fAF67E2aD7C3';
+const deployedAddress = '0x90D3FC3b4f829F32c30C46a721BA9927b716C621';
 const abi = require("../../BetsAbi.json");
 const BetsContract = new web3.eth.Contract(abi, deployedAddress);
 BetsContract.handleRevert = true;
